de9513aaf5db4ff015eea925413e6a1e0b34900b
Daniel <danielfalcaoviktorio@gmail.com>
2024-07-18 13:49:16 -0300

    feat: ensure getStringInfo return uppercase string

---
+++ TS-TDD/src/app/Utils.ts
@@ -0,0 +1,20 @@
+export function toUppercase(text: string) {
+    return text.toUpperCase()
+}
+
+export type StringInfo = {
+    lowerCase: string,
+    upperCase: string,
+    characters: string[],
+    length: number,
+    extraInfo?: Object
+}
+
+export function getStringInfo(text: string): StringInfo {
+    return {
+        lowerCase: text.toLowerCase(),
+        upperCase: text.toUpperCase(),
+        characters: Array.from(text),
+        length: text.length
+    }
+}
