a96f77d8f2e7f08e8f4ec1b6c0d60a89560733f5
Daniel <danielfalcaoviktorio@gmail.com>
2024-07-19 18:32:49 -0300

    refactor: change programming logic

--- TS-TDD/src/app/password_checker/password.ts
+++ TS-TDD/src/app/password_checker/password.ts
@@ -3,18 +3,16 @@ interface IPasswordChecker {
 }
 
 export class PasswordChecker implements IPasswordChecker {
-    private readonly uppercaseRegex = /[A-Z]/g
-    private readonly lowercaseRegex = /[a-z]/g
     check(password: string): boolean {
         if (password.length < 8) {
             return false
         }
 
-        if (!this.uppercaseRegex.test(password)) {
+        if (password === password.toLowerCase()) {
             return false
         }
 
-        if (!this.lowercaseRegex.test(password)) {
+        if (password === password.toUpperCase()) {
             return false
         }
     }
