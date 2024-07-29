6d4260f1f7ac7de8be58097955009243c6d65270
Daniel <danielfalcaoviktorio@gmail.com>
2024-07-29 19:07:06 -0300

    test: ensure return an error if any validation fail

---
+++ SOLID-TDD2/src/controllers/helpers/validators/validation-composite.spec.ts
@@ -0,0 +1,36 @@
+import { IValidation } from "../../protocols/validator"
+import { ValidationComposite } from "./validation-composite"
+
+interface SutTypes {
+    validators: IValidation[]
+    sut: ValidationComposite
+}
+
+const makeSut = (): SutTypes => {
+    const validators: IValidation[] = []
+    validators.push(makeValidationStub())
+    validators.push(makeValidationStub())
+    const sut = new ValidationComposite(validators)
+    return {
+        sut,
+        validators
+    }
+}
+
+const makeValidationStub = (): IValidation => {
+    class ValidationStub implements IValidation {
+        validate(data: any): Error | null {
+            return null
+        }
+    }
+    return new ValidationStub()
+}
+
+describe('Validation Composite', () => {
+    it('Should return an error if any validation fail', () => {
+        const { sut, validators } = makeSut()
+        jest.spyOn(validators[0], 'validate').mockReturnValueOnce(new Error())
+        const error = sut.validate({ field: 'any_field' })
+        expect(error).toEqual(new Error())
+    })
+})
--- SOLID-TDD2/src/controllers/signup/signup.ts
+++ SOLID-TDD2/src/controllers/signup/signup.ts
@@ -1,5 +1,5 @@
 import { MissingParamError } from "../errors/missing-param-error";
-import { badRequest } from "../helpers/http-helpers";
+import { badRequest } from "../helpers/http/http-helpers";
 import { Controller } from "../protocols/controller";
 import { HttpRequest, HttpResponse } from "../protocols/http";
 
