5dfa2954756f3a14f6262da5df35cad66655f112
Daniel <danielfalcaoviktorio@gmail.com>
2024-07-30 15:21:54 -0300

    test: ensure requiredFieldValidator not return if field was provide

--- SOLID-TDD2/src/controllers/helpers/validators/required-fields-validator.spec.ts
+++ SOLID-TDD2/src/controllers/helpers/validators/required-fields-validator.spec.ts
@@ -4,12 +4,20 @@ import { RequiredFieldValidator } from "./required-field-validator"
 describe('RequiredFields Validator', () => {
     it('Should return an error if no field was provide', () => {
         const sut = new RequiredFieldValidator('field')
-        const httpRequest = {
+        const request = {
             email: 'any_mail',
             password: 'any_password',
             confirmPassword: 'any_password'
         }
-        const error = sut.validate(httpRequest)
+        const error = sut.validate(request)
         expect(error).toEqual(new MissingParamError('field'))
+    })
+    it('Should return null if field was provide', () => {
+        const sut = new RequiredFieldValidator('field')
+        const request = {
+            field: 'any_field'
+        }
+        const error = sut.validate(request)
+        expect(error).toBeFalsy()
     })
 })
