9ba3d6fabe50d036492f0c12a5a65951ae25140d
Daniel <danielfalcaoviktorio@gmail.com>
2024-07-30 14:33:55 -0300

    refactor: remove duplicated code

--- SOLID-TDD2/src/controllers/helpers/validators/required-fields-validator.spec.ts
+++ SOLID-TDD2/src/controllers/helpers/validators/required-fields-validator.spec.ts
@@ -2,24 +2,14 @@ import { MissingParamError } from "../../errors"
 import { RequiredFieldValidator } from "./required-field-validator"
 
 describe('RequiredFields Validator', () => {
-    it('Should return an error if no name was provide', () => {
-        const sut = new RequiredFieldValidator('name')
+    it('Should return an error if no field was provide', () => {
+        const sut = new RequiredFieldValidator('field')
         const httpRequest = {
             email: 'any_mail',
             password: 'any_password',
             confirmPassword: 'any_password'
         }
         const error = sut.validate(httpRequest)
-        expect(error).toEqual(new MissingParamError('name'))
-    })
-    it('Should return an error if no email was provide', () => {
-        const sut = new RequiredFieldValidator('email')
-        const httpRequest = {
-            name: 'any_name',
-            password: 'any_password',
-            confirmPassword: 'any_password'
-        }
-        const error = sut.validate(httpRequest)
-        expect(error).toEqual(new MissingParamError('email'))
+        expect(error).toEqual(new MissingParamError('field'))
     })
 })
