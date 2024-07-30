781369785c231ef9834be74de60345a62525eea7
Daniel <danielfalcaoviktorio@gmail.com>
2024-07-30 19:44:05 -0300

    test: ensure PgAccountRepository return an account on succeed

---
+++ SOLID-TDD2/src/infra/db/postgres/account/account-repository.spec.ts
@@ -0,0 +1,35 @@
+import { IAddAccountModel } from "../../../../domain/models/add-account"
+import { IAccountModel } from "../../../../domain/protocols/account"
+import { PgHelper } from "../../helpers/pg-helper"
+import { PgAccountRepository } from "./account-repository"
+
+const makeSut = (): PgAccountRepository => {
+    const sut = new PgAccountRepository()
+    return sut
+}
+
+const makeFakeAccount = (): IAddAccountModel => ({
+    email: 'any_mail@mail.com',
+    name: 'any_name',
+    password: 'hashed_password'
+})
+
+describe('PgAccountRepository', () => {
+    beforeAll(async () => {
+        PgHelper.connect()
+    })
+    beforeEach(async () => {
+        await PgHelper.client.query('DELETE FROM users')
+    })
+    afterAll(async () => {
+        PgHelper.disconnect()
+    })
+    it('Should return an account on succeed', async () => {
+        const sut = makeSut()
+        const account = await sut.add(makeFakeAccount())
+        expect(account.id).toBeTruthy()
+        expect(account.email).toBe('any_mail@mail.com')
+        expect(account.name).toBe('any_name')
+        expect(account.password).toBe('hashed_password')
+    })
+})
