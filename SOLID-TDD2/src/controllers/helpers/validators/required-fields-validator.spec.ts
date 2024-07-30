e5883dbd3c44f4b459f30d0a3ac090b5afc5c973
Daniel <danielfalcaoviktorio@gmail.com>
2024-07-30 14:33:13 -0300

    test: ensure return an error if no email was provide

--- SOLID-TDD2/src/controllers/helpers/validators/required-fields-validator.spec.ts
+++ SOLID-TDD2/src/controllers/helpers/validators/required-fields-validator.spec.ts
@@ -5,13 +5,21 @@ describe('RequiredFields Validator', () => {
     it('Should return an error if no name was provide', () => {
         const sut = new RequiredFieldValidator('name')
         const httpRequest = {
-            body: {
-                email: 'any_mail',
-                password: 'any_password',
-                confirmPassword: 'any_password'
-            }
+            email: 'any_mail',
+            password: 'any_password',
+            confirmPassword: 'any_password'
         }
         const error = sut.validate(httpRequest)
         expect(error).toEqual(new MissingParamError('name'))
+    })
+    it('Should return an error if no email was provide', () => {
+        const sut = new RequiredFieldValidator('email')
+        const httpRequest = {
+            name: 'any_name',
+            password: 'any_password',
+            confirmPassword: 'any_password'
+        }
+        const error = sut.validate(httpRequest)
+        expect(error).toEqual(new MissingParamError('email'))
     })
 })
