2b4a577ea5af1988c1422e0759a64faab1eac2d6
Daniel <danielfalcaoviktorio@gmail.com>
2024-07-30 16:44:09 -0300

    test: ensure DbAddAccount call hasher with correct values

---
+++ SOLID-TDD2/src/data/usecases/add-account/db-add-account.spec.ts
@@ -0,0 +1,62 @@
+import { IAddAccount, IAddAccountModel } from '../../../domain/models/add-account'
+import { IAccountModel } from '../../../domain/protocols/account'
+import { IAddAccountRepository } from '../../protocols/iadd-account-repository'
+import { IHasher } from '../../protocols/ihasher'
+import { DbAddAccount } from './db-add-account'
+
+interface SutTypes {
+    sut: DbAddAccount
+    hasherStub: IHasher
+    addAccountRepoStub: IAddAccount
+}
+
+const makeSut = (): SutTypes => {
+    const hasherStub = makeHasherStub()
+    const addAccountRepoStub = makeAddAccountRepoStub()
+    const sut = new DbAddAccount(hasherStub, addAccountRepoStub)
+    return {
+        sut,
+        hasherStub,
+        addAccountRepoStub
+    }
+}
+
+const makeHasherStub = (): IHasher => {
+    class HasherStub implements IHasher {
+        hash(value: string): Promise<string> {
+            return new Promise(resolve => resolve('hashed_value'))
+        }
+    }
+    return new HasherStub()
+}
+
+const makeAddAccountRepoStub = (): IAddAccountRepository => {
+    class AddAccountStubRepo implements IAddAccountRepository {
+        add(account: IAddAccountModel): Promise<IAccountModel> {
+            return new Promise(resolve => resolve(makeFakeAccount()))
+        }
+    }
+    return new AddAccountStubRepo()
+}
+
+const makeFakeAccount = (): IAccountModel => ({
+    id: 'any_id',
+    email: 'any_mail@mail.com',
+    name: 'any_name',
+    password: 'hashed_value'
+})
+
+const makeFakeRequest = (): IAddAccountModel => ({
+    email: 'any_mail@mail.com',
+    name: 'any_name',
+    password: 'any_password'
+})
+
+describe('DbAddAccount', () => {
+    it('Should call hasher with correct value', async () => {
+        const { sut, hasherStub } = makeSut()
+        const hasherSpy = jest.spyOn(hasherStub, 'hash')
+        await sut.add(makeFakeRequest())
+        expect(hasherSpy).toHaveBeenCalledWith('any_password')
+    })
+})
