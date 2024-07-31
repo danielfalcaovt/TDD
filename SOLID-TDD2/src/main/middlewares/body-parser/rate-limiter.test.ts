288a6d1fa13e74eb1c36065ca16420b205a18032
Daniel <danielfalcaoviktorio@gmail.com>
2024-07-31 14:49:39 -0300

    test: ensure return 200 while not breach limit

--- SOLID-TDD2/src/main/middlewares/body-parser/rate-limiter.test.ts
+++ SOLID-TDD2/src/main/middlewares/body-parser/rate-limiter.test.ts
@@ -3,26 +3,14 @@ import app from '../../config/app'
 import { Request, Response } from 'express'
 
 describe('rateLimiter', () => {
-    beforeAll(() => {
-        app.get('/rate_limit_test', (req: Request, res: Response) => {
+    it('Should return 429 if the rate limit is breached.', async () => {
+        app.get('/rate_limit_breached', (req: Request, res: Response) => {
             res.send('')
         })
-    })
-    it('Should allow request within limit', async () => {
-        for (let i = 0; i < 25; i++) {
-            const response = await request(app).get('/rate_limit_test')
-            expect(response.statusCode).toBe(200)
-        }
-    })
-    it('Should return 429 if the rate limit is breached.', async () => {
-        for (let i = 0; i < 101; i++) {
-            const response = await request(app).get('/rate_limit_test')
-            if (i > 100) {
-                expect(response.statusCode).toBe(429)
-
-            }else {
-                expect(response.statusCode).toBe(200)
-            }
+        for (let i = 0; i < 100; i++) {
+            await request(app)
+            .get('/rate_limit_breached')
+            .expect(200)
         }
     })
 })
