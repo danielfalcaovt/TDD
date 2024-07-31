277073ba8d7c37e2ecca47a79bb5f08143833ec0
Daniel <danielfalcaoviktorio@gmail.com>
2024-07-31 13:16:31 -0300

    test: ensure BodyParser parser body as json

--- SOLID-TDD2/src/main/middlewares/body-parser/body-parser.test.ts
+++ SOLID-TDD2/src/main/middlewares/body-parser/body-parser.test.ts
@@ -1,5 +1,15 @@
+import app from "../../config/app"
+import { Request, Response } from 'express'
+import request from 'supertest'
+
 describe('BodyParser', () => {
     it('Should parse body as json', async () => {
-        
+        app.post('/body_parser_test', (req: Request, res: Response) => {
+            return res.send(req.body)
+        })
+        await request(app)
+        .post('/body_parser_test')
+        .send({ hello: 'world' })
+        .expect({ hello: 'world' })
     })
 })
