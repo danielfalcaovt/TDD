90d0005591fd52c7842afd1082122e2f6196539a
Daniel <danielfalcaoviktorio@gmail.com>
2024-07-31 13:26:35 -0300

    feat: ensure ContentType default type be json

--- SOLID-TDD2/src/main/config/middlewares.ts
+++ SOLID-TDD2/src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express'
 import bodyParser from '../middlewares/body-parser/body-parser'
+import contentType from '../middlewares/body-parser/content-type'
 
 export default function middlewares (app: Express): void {
     app.use(bodyParser)
+    app.use(contentType)
 }
