dcbdb736f01e45d4fcd8d8c890b95b536ca9210a
Daniel <danielfalcaoviktorio@gmail.com>
2024-07-31 13:53:01 -0300

    test: ensure rateLimiter allow request within limit

--- SOLID-TDD2/src/main/middlewares/body-parser/rate-limiter.test.ts
+++ SOLID-TDD2/src/main/middlewares/body-parser/rate-limiter.test.ts
@@ -3,13 +3,26 @@ import app from '../../config/app'
 import { Request, Response } from 'express'
 
 describe('rateLimiter', () => {
-    it('Should allow request within limit', async () => {
+    beforeAll(() => {
         app.get('/rate_limit_test', (req: Request, res: Response) => {
             res.send('')
         })
+    })
+    it('Should allow request within limit', async () => {
         for (let i = 0; i < 25; i++) {
             const response = await request(app).get('/rate_limit_test')
             expect(response.statusCode).toBe(200)
+        }
+    })
+    it('Should return 429 if the rate limit is breached.', async () => {
+        for (let i = 0; i < 101; i++) {
+            const response = await request(app).get('/rate_limit_test')
+            if (i > 100) {
+                expect(response.statusCode).toBe(429)
+
+            }else {
+                expect(response.statusCode).toBe(200)
+            }
         }
     })
 })
