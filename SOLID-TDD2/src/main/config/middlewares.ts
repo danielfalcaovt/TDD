9cdb37362dbfdd8689e092d49a5556917d3f9530
Daniel <danielfalcaoviktorio@gmail.com>
2024-07-31 13:49:19 -0300

    feat: ensure rateLimiter allow request within limit

--- SOLID-TDD2/src/main/config/middlewares.ts
+++ SOLID-TDD2/src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express'
 import bodyParser from '../middlewares/body-parser/body-parser'
 import contentType from '../middlewares/body-parser/content-type'
+import { limiter } from '../middlewares/body-parser/rate-limiter'
 
 export default function middlewares (app: Express): void {
-    app.use(bodyParser)
     app.use(contentType)
+    app.use(bodyParser)
+    app.use(limiter)
 }
