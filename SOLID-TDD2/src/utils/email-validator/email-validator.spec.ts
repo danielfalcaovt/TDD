d4d354c3a3aa804f31f3dd7dfb21bdbada93d89d
Daniel <danielfalcaoviktorio@gmail.com>
2024-07-31 15:32:06 -0300

    test: ensure return false if isEmail returns false

--- SOLID-TDD2/src/utils/email-validator/email-validator.spec.ts
+++ SOLID-TDD2/src/utils/email-validator/email-validator.spec.ts
@@ -1,10 +1,16 @@
-import { EmailValidation } from "../../controllers/helpers/validators/email-validation"
 import { EmailValidator } from "./email-validator"
+import validator from "validator"
 
 describe('EmailValidator', () => {
     it ('Should return true if isEmail returns true', async () => {
         const sut = new EmailValidator()
         const response = sut.isValid('any_mail@mail.com')
         expect(response).toBe(true)
+    })
+    it('Should return false if isEmail returns false', async () => {
+        const sut = new EmailValidator()
+        jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+        const response = sut.isValid('any_mail')
+        expect(response).toBe(false)
     })
 })
