c9c2184756b44071e4ec05a3adb40770f4dc7422
Daniel <danielfalcaoviktorio@gmail.com>
2024-07-31 14:51:23 -0300

    feat: ensure cors access-control-allow-origin be *

--- SOLID-TDD2/src/main/config/middlewares.ts
+++ SOLID-TDD2/src/main/config/middlewares.ts
@@ -2,9 +2,11 @@ import { Express } from 'express'
 import bodyParser from '../middlewares/body-parser/body-parser'
 import contentType from '../middlewares/body-parser/content-type'
 import { limiter } from '../middlewares/body-parser/rate-limiter'
+import cors from '../middlewares/body-parser/cors'
 
 export default function middlewares (app: Express): void {
     app.use(contentType)
     app.use(bodyParser)
     app.use(limiter)
+    app.use(cors)
 }
