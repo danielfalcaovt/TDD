8167f1e0ae61d604302a6ee72c46f1152635572f
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-01 17:00:33 -0300

    feat: ensure return false if compare fails

--- SOLID-TDD2/src/infra/criptography/bcrypt-adapter.ts
+++ SOLID-TDD2/src/infra/criptography/bcrypt-adapter.ts
@@ -1,10 +1,16 @@
-import { IHasher } from "../../data/protocols/ihasher";
+import { IHashComparer } from "../../data/protocols/criptography/icomparer";
+import { IHasher } from "../../data/protocols/criptography/ihasher";
 import bcrypt from 'bcrypt'
 
-export class BcryptAdapter implements IHasher {
+export class BcryptAdapter implements IHasher, IHashComparer {
     constructor(private readonly salt: number) {}
     async hash(value: string): Promise<string> {
         const hashedValue = await bcrypt.hash(value, this.salt)
         return hashedValue
+    }
+
+    async compare(value: string, valueToCompare: string): Promise<boolean> {
+        const result = await bcrypt.compare(value, valueToCompare)
+        return result
     }
 }
