77e870699fe7f55bc8e9bb8fba4fb7eb0149a77d
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-01 17:22:36 -0300

    test: ensure JwtAdapter throw if sign throws

--- SOLID-TDD2/src/infra/criptography/jwt/jwt-adapter.spec.ts
+++ SOLID-TDD2/src/infra/criptography/jwt/jwt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import jwt from 'jsonwebtoken'
 import { JwtAdapter } from './jwt-adapter'
 
+jest.mock('jsonwebtoken', () => ({
+    sign() {
+        return new Promise(resolve => resolve('any_token'))
+    }
+}))
+
 describe('JwtAdapter', () => {
     const jwtSecret = 'secret'
     it ('Should call sign with correct values', async () => {
@@ -8,5 +14,15 @@ describe('JwtAdapter', () => {
         const signSpy = jest.spyOn(jwt, 'sign')
         await sut.generate('any_id')
         expect(signSpy).toHaveBeenCalledWith({id: 'any_id'}, jwtSecret)
+    })
+    it('Should throw if sign throws', async () => {
+        const sut = new JwtAdapter(jwtSecret)
+        jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+            return new Promise((resolve, reject) => {
+                reject(new Error())
+            })
+        })
+        const response =  sut.generate('any_id')
+        await expect(response).rejects.toThrow()
     })
 })
