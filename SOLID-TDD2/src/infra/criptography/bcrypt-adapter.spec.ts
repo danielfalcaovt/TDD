54afa700bf0d2a25829a716dc2d0783af1a9bac6
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-01 17:00:44 -0300

    test: ensure return false if compare fails

--- SOLID-TDD2/src/infra/criptography/bcrypt-adapter.spec.ts
+++ SOLID-TDD2/src/infra/criptography/bcrypt-adapter.spec.ts
@@ -4,6 +4,9 @@ import { BcryptAdapter } from './bcrypt-adapter'
 jest.mock('bcrypt', () => ({
     hash() {
         return new Promise(resolve => resolve('hashed_value'))
+    },
+    compare() {
+        return new Promise(resolve => resolve(true))
     }
 }))
 
@@ -28,5 +31,13 @@ describe('Bcrypt Adapter', () => {
         const sut = new BcryptAdapter(salt)
         const hash = await sut.hash('any_value')
         expect(hash).toBe('hashed_value')
+    })
+    it('Should return false if compare fails', async () => {
+        const sut = new BcryptAdapter(salt)
+        jest.spyOn(bcrypt, 'compare').mockImplementationOnce(() => {
+            return new Promise(resolve => resolve(false))
+        })
+        const comparison = await sut.compare('any_value', 'another_value')
+        expect(comparison).toBe(false)
     })
 })
